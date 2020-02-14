3b1e3884da2b48d31e0776b955adc2c3bdc2b6bd
shadyaziza <shadyaziza@gmail.com>
2020-02-14 10:23:38 +0200

    Add appbar to home screen

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key key}) : super(key: key);
 
   @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  PageController _pageCont;
+  int _index = 0;
+  List<String> _tabs = ['Rooms', 'Schedule', 'Power'];
+  @override
+  void initState() {
+    _pageCont = PageController();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(_tabs[_index]),
+        elevation: 2.0,
+        leading: Container(),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+    );
   }
 }
