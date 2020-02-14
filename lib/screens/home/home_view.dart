0bfb5c7085f57d5b86b0413b4674c4c2b1ef1cce
shadyaziza <shadyaziza@gmail.com>
2020-02-14 15:18:30 +0200

    Add more screen

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wamda/screens/home/more.dart';
 import 'package:wamda/screens/home/rooms.dart';
 import 'package:wamda/screens/home/schdeules.dart';
 
@@ -55,9 +56,7 @@ class _HomeViewState extends State<HomeView> {
           Container(
             color: Colors.red,
           ),
-          Container(
-            color: Colors.blueGrey,
-          )
+          MoreScreen()
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
